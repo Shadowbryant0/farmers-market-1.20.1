0dd0ca21d363446e124188a7faec5fc49ab20f55
Shadowbryant0 <shadowbryant0@gmail.com>
2025-05-10 01:52:57 -0500

    update

--- src/main/java/net/shadow/farmersmarket/item/custom/RapierWeaponItem.java
+++ src/main/java/net/shadow/farmersmarket/item/custom/RapierWeaponItem.java
@@ -1,65 +1,51 @@
 package net.shadow.farmersmarket.item.custom;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
-import net.minecraft.util.UseAction;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.dimension.DimensionTypes;
 
 public class RapierWeaponItem extends Item {
-    public RapierWeaponItem(Item.Settings settings) {
-        super(settings);
+    public RapierWeaponItem(Settings settings) {
+        super(settings.maxCount(1));
     }
 
+    double boost = 4.0d;
+    double notRightDimensionDebuff = 1.0d;
     @Override
-    public int getMaxUseTime(ItemStack stack) {
-        return 20;
-    }
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (!world.isClient){
+            Vec3d lookingDirection = user.getRotationVec(1.0f);
+            RegistryKey<DimensionType> the_nether = DimensionTypes.THE_NETHER;
+                user.setVelocity(
+                        lookingDirection.x * boost,
+                        lookingDirection.y * boost * 0.6f,
+                        lookingDirection.z * boost
+                );
 
-    @Override
-    public UseAction getUseAction(ItemStack stack) {
-        onUse;
-        if (entity == null) return;
+                user.setVelocity(
+                        lookingDirection.x * boost / notRightDimensionDebuff,
+                        lookingDirection.y * boost * 0.6f/ notRightDimensionDebuff,
+                        lookingDirection.z * boost / notRightDimensionDebuff
+                );
 
-        double length = 0;
-
-        Vec3d look = entity.getRotationVec(1.0F);
-        entity.setVelocity(look.x * 2.5, look.y * 2.0, look.z * 2.5);
-        entity.velocityModified = true;
-
-        if (entity instanceof PlayerEntity player) {
-            if (player.isFallFlying()) {
-                player.getItemCooldownManager().set(ItemStack.getItem(), 1000);
+            user.velocityModified = true;
+            user.setSwimming(false);
+        }
+            if (user.isFallFlying()) {
+                user.getItemCooldownManager().set(ItemStack.class(RapierWeaponItem), 1000;
             } else {
-                player.getItemCooldownManager().set(ItemStack.getItem(), 200);
-            }
-        }
-
-        for (int i = 0; i < 10; i++) {
-            Vec3d center = new Vec3d(
-                    x + look.x * length,
-                    y + 1.6 + look.y * length,
-                    z + look.z * length
-            );
-
-            List<Entity> foundEntities = world.getOtherEntities(
-                            entity,
-                            new Box(center.subtract(1, 1, 1), center.add(1, 1, 1)),
-                            e -> e instanceof LivingEntity
-                    ).stream().sorted(Comparator.comparingDouble(e -> e.squaredDistanceTo(center)))
-                    .collect(Collectors.toList());
-
-            for (entity target : foundEntities) {
-                if (!target.equals(entity) && target instanceof LivingEntity livingTarget && !world.isClient) {
-                    livingTarget.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 80, 0, false, true));
-                    livingTarget.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, 80, 0, false, true));
-                }
+                user.getItemCooldownManager().set(ItemStack.class(RapierWeaponItem), 200;
             }
 
-            length += 1;
-        }
-    }`
-
+        return super.use(world, user, hand);
     }
-    onUse
+}
