9b6a06cd1f37550d3a2f049c0aecad96d534ed34
Shadowbryant0 <shadowbryant0@gmail.com>
2025-05-25 04:08:35 -0500

    update - shadow

--- src/main/java/net/shadow/farmersmarket/item/ModItems.java
+++ src/main/java/net/shadow/farmersmarket/item/ModItems.java
@@ -2,9 +2,7 @@ package net.shadow.farmersmarket.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.item.MusicDiscItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
