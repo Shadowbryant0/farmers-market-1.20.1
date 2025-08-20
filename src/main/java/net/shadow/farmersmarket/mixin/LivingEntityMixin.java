e956c87746879f8aeeb546a410792fff33956480
shadow <joejoe112506@gmail.com>
2025-08-19 21:33:18 -0500

    update 1.0.93 - shadow

--- src/main/java/net/shadow/farmersmarket/mixin/LivingEntityMixin.java
+++ src/main/java/net/shadow/farmersmarket/mixin/LivingEntityMixin.java
@@ -23,24 +23,83 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-class SliverOfFleshMixin {
+class PlayerDropsMixin {
 
     @Inject(method = "onDeath", at = @At("HEAD"))
     private void onDeathInject(DamageSource source, CallbackInfo ci) {
-        if (!(source.getAttacker() instanceof PlayerEntity attacker)) return;
-        if (!(((Object) this) instanceof PlayerEntity victim)) return;
+        if (!(source.getAttacker() instanceof LivingEntity attacker)) return;
+        if (!(((Object) this) instanceof LivingEntity victim)) return;
 
         ItemStack weapon = attacker.getMainHandStack();
-        if (EnchantmentHelper.getLevel(FarmersMarketEnchants.HuntersLullabyEnchantment, weapon) > 0 || EnchantmentHelper.getLevel(FarmersMarketEnchants.PrimalDesires, weapon) > 0) {
+        if (EnchantmentHelper.getLevel(FarmersMarketEnchants.PrimalDesires, weapon) > 0) {
             World world = attacker.getWorld();
-            if (!world.isClient) {
+            if (victim instanceof PlayerEntity) {
+
                 // Drop Silver Flesh
                 ItemStack sliverFlesh = new ItemStack(ModItems.SLIVER_FLESH);
+                Vec3d pos = attacker.getPos();
+
+                world.spawnEntity(new ItemEntity(world, pos.x, pos.y, pos.z, sliverFlesh));
+
+            } else {
+
+
+                // Drop Cracked Skull
+                ItemStack crackedSkull = new ItemStack(Items.ROTTEN_FLESH);
+                Vec3d pos = victim.getPos();
+
+                world.spawnEntity(new ItemEntity(world, pos.x, pos.y, pos.z, crackedSkull));
+
+
+            }
+        }
+
+        if (EnchantmentHelper.getLevel(FarmersMarketEnchants.HuntersLullabyEnchantment, weapon) > 0) {
+            World world = attacker.getWorld();
+            if (victim instanceof PlayerEntity) {
+System.out.println("Player killed");
+                // Drop Silver Flesh
+                ItemStack sliverFlesh = new ItemStack(Items.DIAMOND_BLOCK);
                 Vec3d pos = victim.getPos();
 
                 world.spawnEntity(new ItemEntity(world, pos.x, pos.y, pos.z, sliverFlesh));
+
+            } else {
+
+
+                // Drop Cracked Skull
+                ItemStack crackedSkull = new ItemStack(ModItems.SLIVER_FLESH);
+                Vec3d pos = attacker.getPos();
+
+                world.spawnEntity(new ItemEntity(world, pos.x, pos.y, pos.z, crackedSkull));
+
+
             }
         }
+        if (EnchantmentHelper.getLevel(FarmersMarketEnchants.JagerderSchuldigen, weapon) > 0) {
+            World world = attacker.getWorld();
+            if (victim instanceof PlayerEntity) {
+
+                // Drop Silver Flesh
+                ItemStack sliverFlesh = new ItemStack(ModItems.CRACKED_SKULL);
+                Vec3d pos = attacker.getPos();
+
+                world.spawnEntity(new ItemEntity(world, pos.x, pos.y, pos.z, sliverFlesh));
+
+            } else
+                if (victim instanceof WitherSkeletonEntity) {if (attacker.getRandom().nextInt(10) == 10) {
+
+                    // Drop Cracked Skull
+                    ItemStack crackedSkull = new ItemStack(Items.WITHER_SKELETON_SKULL);
+                    Vec3d pos = attacker.getPos();
+
+                    world.spawnEntity(new ItemEntity(world, pos.x, pos.y, pos.z, crackedSkull));
+
+                }
+            }
+        }
+
+
     }
 }
 @Mixin(LivingEntity.class)
@@ -56,70 +115,42 @@ class StarvationMixin {
            // beserk - enchancement by MoriyaShiine
 }
 @Mixin(LivingEntity.class)
-class StarvationKillMixin {
-
- @Inject(method = "onDeath", at = @At("HEAD"))
-private void onDeathInject(DamageSource source, CallbackInfo ci) {
-    if (!(source.getAttacker() instanceof LivingEntity attacker)) return;
-
-
-    ItemStack weapon = attacker.getMainHandStack();
-    if (EnchantmentHelper.getLevel(FarmersMarketEnchants.Starvation, weapon) > 0) {
-        World world = attacker.getWorld();
-        if (!world.isClient) {
-            if (attacker instanceof PlayerEntity player){
-                player.getHungerManager().add(8, .2f);
-            } else {
-                attacker.heal(6);
-            }
-
-
-
-      }
-    }
-    }
-}
-
-@Mixin(LivingEntity.class)
-class JagerDamageMixin {
-    @ModifyVariable(method = "damage", at = @At("HEAD"), argsOnly = true)
-    private float FarmersMarket$Starvation(float value, DamageSource source) {
-
-        if (source.getSource() instanceof LivingEntity living && !living.getWorld().isClient) {
-            return value + FarmersmarketUtil.JagerDamage(living, living.getMainHandStack());
-        }
-        return value;
-    }
-    // beserk - enchancement by MoriyaShiine
-}
-@Mixin(LivingEntity.class)
-class JagerKillMixin {
-
+class StarvationkilMixin {
     @Inject(method = "onDeath", at = @At("HEAD"))
     private void onDeathInject(DamageSource source, CallbackInfo ci) {
-        if (!(source.getAttacker() instanceof PlayerEntity attacker)) return;
+        if (!(source.getAttacker() instanceof LivingEntity attacker)) return;
         if (!(((Object) this) instanceof LivingEntity victim)) return;
-
         ItemStack weapon = attacker.getMainHandStack();
-        if (EnchantmentHelper.getLevel(FarmersMarketEnchants.JagerderSchuldigen, weapon) > 0 || EnchantmentHelper.getLevel(FarmersMarketEnchants.PrimalDesires, weapon) > 0) {
+        if (EnchantmentHelper.getLevel(FarmersMarketEnchants.Starvation, weapon) > 0) {
             World world = attacker.getWorld();
-            if(victim instanceof PlayerEntity){
             if (!world.isClient) {
-                // Drop Cracked Skull
-                ItemStack crackedSkull = new ItemStack(ModItems.CRACKED_SKULL);
-                Vec3d pos = victim.getPos();
+                if (attacker instanceof PlayerEntity player) {
+                    if (victim instanceof PlayerEntity) {
+                        player.getHungerManager().add(12, .4f);
+                    } else {
+                        player.getHungerManager().add(8, .2f);
+                    }
+                } else {
 
-                world.spawnEntity(new ItemEntity(world, pos.x, pos.y, pos.z, crackedSkull));
-            }} else if (victim instanceof WitherSkeletonEntity){
-                if (attacker.getRandom().nextInt(12) == 10){
-                if (!world.isClient) {
-                    // Drop Cracked Skull
-                    ItemStack crackedSkull = new ItemStack(Items.WITHER_SKELETON_SKULL);
-                    Vec3d pos = victim.getPos();
 
-                    world.spawnEntity(new ItemEntity(world, pos.x, pos.y, pos.z, crackedSkull));
-                }}
+                    attacker.heal(6);
+                }
 
+
+            }
         }
     }
-}}
+}
+        @Mixin(LivingEntity.class)
+        class JagerDamageMixin {
+            @ModifyVariable(method = "damage", at = @At("HEAD"), argsOnly = true)
+            private float FarmersMarket$Starvation(float value, DamageSource source) {
+
+                if (source.getSource() instanceof LivingEntity living && !living.getWorld().isClient) {
+                    return value + FarmersmarketUtil.JagerDamage(living, living.getMainHandStack());
+                }
+                return value;
+            }
+            // beserk - enchancement by MoriyaShiine
+        }
+
